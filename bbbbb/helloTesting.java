a5a5194f4b75deb8bb025476b4fa6ef8efb21cf2
Donelys Familia <donelysprivate@gmail.com>
2019-08-12 13:51:12 -0400

    method added

---
+++ bbbbb/helloTesting.java
@@ -0,0 +1,19 @@
+public class helloTesting {
+
+    public static void main(String[] args) {
+        System.out.println("Hello Testing");
+        even(5);
+
+    }
+
+    public static Boolean even(int numb) {
+
+        if (numb % 2 == 0) {
+            System.out.println("Number " + numb + "is even");
+            return true;
+        } else {
+            System.out.println("Number " + numb + "is odd");
+            return false;
+        }
+    }
+}
